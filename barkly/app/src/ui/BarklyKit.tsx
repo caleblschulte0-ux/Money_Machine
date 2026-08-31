9faa1c84674431f5363efff856d61a54be5b6383
caleblschulte0-ux <caleblschulte0@gmail.com>
2026-08-31 13:42:51 -0500

    Barkly layout: clear final NPC care-dock edge

--- barkly/app/src/ui/BarklyKit.tsx
+++ barkly/app/src/ui/BarklyKit.tsx
@@ -146,7 +146,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
     left: INTERACTION_GUTTER,
     right: INTERACTION_GUTTER,
-    bottom: 0,
+    bottom: -2,
     height: CARE_DOCK_HEIGHT,
     flexDirection: 'row',
     justifyContent: 'space-around',
